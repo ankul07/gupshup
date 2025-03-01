bf3406d8548685b267bd19c47ddd4f25aab0a3c3
ankulcode <frost.ankur019@gmail.com>
2025-03-02 03:37:47 +0530

    update frontend api route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://gupshup-three.vercel.app/api",
   withCredentials: true, // Important for cookies
 });
 
