228f2358d2901e294bf4fb3ae7ce42fb32792bca
ankulcode <frost.ankur019@gmail.com>
2025-04-23 16:10:45 +0530

    fix errorS

--- server-template/src/controllers/user.controller.js
+++ server-template/src/controllers/user.controller.js
@@ -509,6 +509,8 @@ export const logout = asyncHandler(async (req, res, next) => {
   const options = {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
   res.status(200).cookie("refreshtoken", options).json({
     success: true,
